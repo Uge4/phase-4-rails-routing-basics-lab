9cf588dafe2e42ee1c9109983c8377e622ae7cb7
eugene <eugeneholenstein@gmail.com>
2023-07-13 00:18:32 +1000

    completed lab

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        student_grades = Student.order("grade desc")
+        render json: student_grades
+    end
+end
